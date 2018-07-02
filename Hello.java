f0e338d38e71c2134f05326d6cc12d0c74adb3ff
zhangyanchen1 <40751326+zhangyanchen1@users.noreply.github.com>
2018-07-02 14:57:36 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/*
+Hello.java
+2015-10-9
+v1.0
+*/
+public class Hello{
+         public static void main(String[] args){
+                 //输出控制台信息
+                 System.out.print("hello world!!\t");
+                 System.out.print("hello world2!!!");
+         }
+}
